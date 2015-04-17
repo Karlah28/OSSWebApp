c02e03e05d49ac8ae415f9d2d976ce336b55e5ab
Karla Hernandez <karla.hernandez0728@gmail.com>
2015-04-16 19:14:53 -0500

    Cache stuff

--- AngularJSWithTypeScriptDemo/app/app.module.js
+++ AngularJSWithTypeScriptDemo/app/app.module.js
@@ -1,6 +1,6 @@
 /// <reference path="../scripts/typings/angularjs/angular.d.ts" />
 (function () {
-    var app = angular.module("ossApp", ['ngRoute']);
+    var app = angular.module("ossApp", ['ngRoute', 'ngStorage']);
     app.config(ossApp.Routes.configureRoutes);
 })();
 //# sourceMappingURL=app.module.js.map
