ce2f7b15b6884f8e17937666d0b5c28a16521a58
Karla Hernandez <karla.hernandez0728@gmail.com>
2015-04-26 14:03:42 -0500

    check boxes work when clicking/unclicking in manage users

--- AngularJSWithTypeScriptDemo/app/controllers/ManageUsersController.js
+++ AngularJSWithTypeScriptDemo/app/controllers/ManageUsersController.js
@@ -1,80 +1,3 @@
-//module ossApp.controllers {
-//    export class ManageUsersController {
-//        NavbarService: ossApp.Interfaces.INavbarService;
-//        static $inject = [
-//            "ossApp.Services.NavbarService",
-//            'ossApp.Services.HttpService',
-//            '$scope',
-//            '$localStorage'
-//        ];
-//        constructor(
-//            NavbarService: ossApp.Interfaces.INavbarService,
-//            HttpService: ossApp.Interfaces.HttpService,
-//            public $scope,
-//            $localStorage
-//            ) {
-//            var baseUrl = "http://localhost:51941/api";
-//            //make call on page load
-//            angular.element(document).ready(function () {
-//                var getUsersUrl = "/Account/ViewUnregisteredUsers";
-//                HttpService.serverGet(baseUrl + getUsersUrl,(results) => {
-//                    $localStorage.potentialUsers = results;
-//                    $scope.setProspects();
-//                });
-//            });
-//            var prospects = $localStorage.potentialUsers;
-//            $scope.title = "User Management";
-//            $scope.prospects = new Array<ossApp.Interfaces.ProspectUser>();
-//            $scope.TempProspects = new Array<ossApp.Interfaces.ProspectUser>();
-//            $scope.setProspects = () => {
-//                for (var i = 0; i < $localStorage.potentialUsers.length; i++) {
-//                    $scope.prospects.push($localStorage.potentialUsers[i]);
-//                }
-//            }
-//            //$scope.prospects.push(
-//            //    {
-//            //        FirstName: "Karla",
-//            //        LastName: "Hernandez",
-//            //        Email: "karla.hernandez@mavs.uta.edu",
-//            //        PhoneNumber: "817456623"
-//            //    },
-//            //    {
-//            //        FirstName: "Robert",
-//            //        LastName: "Calvert",
-//            //        Email: "sample@mavs.uta.edu",
-//            //        PhoneNumber: "817456623"
-//            //    },
-//            //    {
-//            //        FirstName: "Caleb",
-//            //        LastName: "Goodman",
-//            //        Email: "caleb@gmail.com",
-//            //        PhoneNumber: "554522150"
-//            //    });
-//            $scope.addTemp = (prospect: any) => {
-//                $scope.TempProspects.push(prospect);
-//            };
-//            //send approved users
-//            $scope.approve = () => {
-//                //clear temp array
-//                var x = $scope.TempProspects;
-//                var methodUrl = "/Account/ViewUnregisteredUsers";
-//                HttpService.serverPost(baseUrl + methodUrl, $scope.TempProspects,(results) => {
-//                    var x = results;
-//                });
-//            };
-//            //send denied users
-//            $scope.deny = () => {
-//                //clear temp array
-//                var x = $scope.TempProspects;
-//                var methodUrl = "/Account/something";
-//                HttpService.serverPost(baseUrl + methodUrl, $scope.TempProspects,(results) => {
-//                    var x = results;
-//                });
-//            };
-//        }
-//    }
-//    angular.module("ossApp").controller("ossApp.controllers.ManageUsersController", ManageUsersController);
-//} 
 var ossApp;
 (function (ossApp) {
     var controllers;
@@ -119,8 +42,9 @@ var ossApp;
                 //        Email: "caleb@gmail.com",
                 //        PhoneNumber: "554522150"
                 //    });
-                $scope.addTemp = function (prospect) {
-                    $scope.TempProspects.push(prospect);
+                $scope.addTemp = function (index) {
+                    //do something in array
+                    //$scope.TempProspects.push(prospect);
                 };
                 //send approved users
                 $scope.approve = function () {
--- AngularJSWithTypeScriptDemo/app/controllers/ManageUsersController.ts
+++ AngularJSWithTypeScriptDemo/app/controllers/ManageUsersController.ts
@@ -1,88 +1,4 @@
-﻿//module ossApp.controllers {
-//    export class ManageUsersController {
-//        NavbarService: ossApp.Interfaces.INavbarService;
-//        static $inject = [
-//            "ossApp.Services.NavbarService",
-//            'ossApp.Services.HttpService',
-//            '$scope',
-//            '$localStorage'
-//        ];
-//        constructor(
-//            NavbarService: ossApp.Interfaces.INavbarService,
-//            HttpService: ossApp.Interfaces.HttpService,
-//            public $scope,
-//            $localStorage
-//            ) {
-//            var baseUrl = "http://localhost:51941/api";
-//            //make call on page load
-//            angular.element(document).ready(function () {
-//                var getUsersUrl = "/Account/ViewUnregisteredUsers";
-//                HttpService.serverGet(baseUrl + getUsersUrl,(results) => {
-//                    $localStorage.potentialUsers = results;
-//                    $scope.setProspects();
-//                });
-//            });
-
-//            var prospects = $localStorage.potentialUsers;
-
-//            $scope.title = "User Management";
-//            $scope.prospects = new Array<ossApp.Interfaces.ProspectUser>();
-//            $scope.TempProspects = new Array<ossApp.Interfaces.ProspectUser>();
-
-//            $scope.setProspects = () => {
-//                for (var i = 0; i < $localStorage.potentialUsers.length; i++) {
-//                    $scope.prospects.push($localStorage.potentialUsers[i]);
-//                }
-//            }
-            
-//            //$scope.prospects.push(
-//            //    {
-//            //        FirstName: "Karla",
-//            //        LastName: "Hernandez",
-//            //        Email: "karla.hernandez@mavs.uta.edu",
-//            //        PhoneNumber: "817456623"
-//            //    },
-//            //    {
-//            //        FirstName: "Robert",
-//            //        LastName: "Calvert",
-//            //        Email: "sample@mavs.uta.edu",
-//            //        PhoneNumber: "817456623"
-//            //    },
-//            //    {
-//            //        FirstName: "Caleb",
-//            //        LastName: "Goodman",
-//            //        Email: "caleb@gmail.com",
-//            //        PhoneNumber: "554522150"
-//            //    });
-
-//            $scope.addTemp = (prospect: any) => {
-//                $scope.TempProspects.push(prospect);
-//            };
-
-//            //send approved users
-//            $scope.approve = () => {
-//                //clear temp array
-//                var x = $scope.TempProspects;
-//                var methodUrl = "/Account/ViewUnregisteredUsers";
-//                HttpService.serverPost(baseUrl + methodUrl, $scope.TempProspects,(results) => {
-//                    var x = results;
-//                });
-//            };
-
-//            //send denied users
-//            $scope.deny = () => {
-//                //clear temp array
-//                var x = $scope.TempProspects;
-//                var methodUrl = "/Account/something";
-//                HttpService.serverPost(baseUrl + methodUrl, $scope.TempProspects,(results) => {
-//                    var x = results;
-//                });
-//            };
-//        }
-//    }
-//    angular.module("ossApp").controller("ossApp.controllers.ManageUsersController", ManageUsersController);
-//} 
-module ossApp.controllers {
+﻿module ossApp.controllers {
     export class ManageUsersController {
         NavbarService: ossApp.Interfaces.INavbarService;
         static $inject = [
@@ -139,8 +55,10 @@ module ossApp.controllers {
             //        PhoneNumber: "554522150"
             //    });
 
-            $scope.addTemp = (prospect: any) => {
-                $scope.TempProspects.push(prospect);
+            $scope.addTemp = (index: any) => {
+
+               //do something in array
+                //$scope.TempProspects.push(prospect);
             };
 
             //send approved users
