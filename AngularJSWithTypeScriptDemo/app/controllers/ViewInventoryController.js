3e4efa60be78196519060fc720a0eb1123336342
Joseph <joseph.finnegan@mavs.uta.edu>
2015-05-01 12:21:18 -0500

    view inventory page finished

--- AngularJSWithTypeScriptDemo/app/controllers/ViewInventoryController.js
+++ AngularJSWithTypeScriptDemo/app/controllers/ViewInventoryController.js
@@ -10,6 +10,72 @@ var ossApp;
                 $scope.sendRequest = function () {
                     window.alert("Request Successfully Sent");
                 };
+                //TOGGLE FUNCTIONS============================================
+                $scope.checkedItems = [];
+                $scope.toggleItem = function (item) {
+                    var found = 0;
+                    var index = 0;
+                    for (var i = 0; i < $scope.checkedItems.length; i++) {
+                        if ($scope.checkedItems[i] == item) {
+                            found = 1;
+                            index = i;
+                        }
+                    }
+                    if (found == 1) {
+                        $scope.checkedItems.splice(index, 1);
+                    }
+                    else {
+                        $scope.checkedItems.push(item);
+                    }
+                };
+                //checkbox toggler used for crates tab
+                $scope.checkedCrates = [];
+                $scope.toggleCrate = function (crate) {
+                    var found = 0;
+                    var index = 0;
+                    for (var i = 0; i < $scope.checkedCrates.length; i++) {
+                        if ($scope.checkedCrates[i] == crate) {
+                            found = 1;
+                            index = i;
+                        }
+                    }
+                    if (found == 1) {
+                        $scope.checkedCrates.splice(index, 1);
+                    }
+                    else {
+                        $scope.checkedCrates.push(crate);
+                    }
+                };
+                //checkbox toggler used for projects tab
+                $scope.checkedProjects = [];
+                $scope.toggleProject = function (project) {
+                    var found = 0;
+                    var index = 0;
+                    for (var i = 0; i < $scope.checkedProjects.length; i++) {
+                        if ($scope.checkedProjects[i] == project) {
+                            found = 1;
+                            index = i;
+                        }
+                    }
+                    if (found == 1) {
+                        $scope.checkedProjects.splice(index, 1);
+                    }
+                    else {
+                        $scope.checkedProjects.push(project);
+                    }
+                };
+                //MODAL ITEMS STUFF===============================================
+                $scope.modalItems = [];
+                $scope.compileModalItems = function (itemList) {
+                    $scope.modalItems = itemList;
+                };
+                $scope.compileModalItems = function (itemList, quantityList) {
+                    $scope.modalItems = [];
+                    for (var i = 0; i < itemList.length; i++) {
+                        $scope.modalItems.push({ ItemName: itemList[i], Quantity: quantityList[i] });
+                    }
+                };
+                //END MODAL ITEMS STUFF===================================
             }
             ViewInventoryController.$inject = [
                 "ossApp.Services.NavbarService",
--- AngularJSWithTypeScriptDemo/app/controllers/ViewInventoryController.ts
+++ AngularJSWithTypeScriptDemo/app/controllers/ViewInventoryController.ts
@@ -23,8 +23,79 @@
             $scope.sendRequest = () => {
                 window.alert("Request Successfully Sent");
             };
-        }
 
+
+            //TOGGLE FUNCTIONS============================================
+            $scope.checkedItems = [];
+            $scope.toggleItem = (item) => {
+                var found = 0
+                var index = 0
+                for (var i = 0; i < $scope.checkedItems.length; i++) {
+                    if ($scope.checkedItems[i] == item) {
+                        found = 1;
+                        index = i;
+                    }
+                }
+                if (found == 1) {
+                    $scope.checkedItems.splice(index, 1);
+                }
+                else {
+                    $scope.checkedItems.push(item);
+                }
+            };
+            //checkbox toggler used for crates tab
+            $scope.checkedCrates = [];
+            $scope.toggleCrate = (crate) => {
+                var found = 0
+                var index = 0
+                for (var i = 0; i < $scope.checkedCrates.length; i++) {
+                    if ($scope.checkedCrates[i] == crate) {
+                        found = 1;
+                        index = i;
+                    }
+                }
+                if (found == 1) {
+                    $scope.checkedCrates.splice(index, 1);
+                }
+                else {
+                    $scope.checkedCrates.push(crate);
+                }
+            };
+
+
+            //checkbox toggler used for projects tab
+            $scope.checkedProjects = [];
+            $scope.toggleProject = (project) => {
+                var found = 0
+                var index = 0
+                for (var i = 0; i < $scope.checkedProjects.length; i++) {
+                    if ($scope.checkedProjects[i] == project) {
+                        found = 1;
+                        index = i;
+                    }
+                }
+                if (found == 1) {
+                    $scope.checkedProjects.splice(index, 1);
+                }
+                else {
+                    $scope.checkedProjects.push(project);
+                }
+            };
+
+            //MODAL ITEMS STUFF===============================================
+            $scope.modalItems = []
+            $scope.compileModalItems = (itemList) => {
+                $scope.modalItems = itemList;
+            };
+
+            $scope.compileModalItems = (itemList, quantityList) => {
+                $scope.modalItems = [];
+                for (var i = 0; i < itemList.length; i++) {
+                    $scope.modalItems.push({ ItemName : itemList[i], Quantity: quantityList[i]});
+                }
+            };
+            //END MODAL ITEMS STUFF===================================
+        }
     }
     angular.module("ossApp").controller("ossApp.controllers.ViewInventoryController", ViewInventoryController);
 }  
